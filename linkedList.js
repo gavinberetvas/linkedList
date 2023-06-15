b3e3466b77e943c47e31b473fb397ad179d85b2b
Gavin <gavinberetvas@ucsb.edu>
2023-06-14 20:59:02 -0700

    main finish. Extra credit start

--- linkedList.js
+++ linkedList.js
@@ -49,10 +49,10 @@ class LinkedList {
         counter++;
       }
 
-      console.log("total items in list", counter)
-      return counter
+      console.log("total items in list", counter);
+      return counter;
     } else {
-      return 0
+      return 0;
     }
   }
 
@@ -75,31 +75,83 @@ class LinkedList {
         current = current.nextNode;
       }
 
-      let lastItem = current
-      console.log("tail of list:", lastItem)
-      return lastItem
+      let lastItem = current;
+      console.log("tail of list:", lastItem);
+      return lastItem;
     }
   }
 
   at(index) {
-    //returns the node at given index? like the number?
+    if (this.head != null) {
+      let current = this.head;
+
+      for (let i = 1; i < index; i++) {
+        current = current.nextNode;
+      }
+      console.log(`item at position ${index}:`, current);
+      return current;
+    } else {
+      return "error";
+    }
   }
 
   pop() {
-    //removes the last element from the list
+    let previousNode = null;
+    if (this.head != null) {
+      let current = this.head;
+      while (current.nextNode != null) {
+        previousNode = current;
+        current = current.nextNode;
+      }
+      previousNode.nextNode = null;
+    }
   }
 
   contains(value) {
-    // returns true if the passed in value is in the list and otherwise returns false.
+    let current = this.head;
+
+    while (current.nextNode != null) {
+      current = current.nextNode;
+      if (current.value == value) {
+        console.log("true");
+        return true;
+      }
+    }
+    console.log("false");
+    return false;
   }
 
   find(value) {
+    let current = this.head;
+    let counter = 1;
+
+    while (current.nextNode != null) {
+      current = current.nextNode;
+      counter++;
+      if (current.value == value) {
+        console.log(`index of ${value}:`, counter);
+        return counter;
+      }
+    }
+    console.log("not found");
+    return "not found";
     // returns the index of the node containing value, or null if not found.
   }
 
   toString() {
-    // represents your LinkedList objects as strings, so you can print them out and preview them in the console. The format should be:
-    // ( value ) -> ( value ) -> ( value ) -> null
+    let current = this.head;
+    let stringedList = "";
+
+    while (current.nextNode != null) {
+      console.log(` ${current.value} ---> `);
+      stringedList += `${current.value} ---> `;
+      current = current.nextNode;
+    }
+
+    stringedList += "null";
+    console.log("null");
+    console.log(`head: ${stringedList}`);
+    return `head: ${stringedList}`;
   }
 
   insertAt(value, index) {
@@ -107,7 +159,51 @@ class LinkedList {
   }
 
   removeAt(index) {
-    //that removes the node at the given index.
+    let previousNode = null;
+    let remainingList = null;
+    let current = this.head;
+    let counter = 1;
+
+    if (index == 1) {
+      this.head = current.nextNode;
+      return this.toString();
+    }
+
+    for (let i = 1; i < index; i++) {
+      previousNode = current;
+      current = current.nextNode;
+      if (current == null) {
+        console.log("error. Item not found")
+        return "error. Item not found"
+      }
+    }
+    previousNode.nextNode = current.nextNode;
+
+    console.log("remove at", this.toString());
+    return this.toString;
+
+    // if (this.head == null) {
+    //   return "empty list"
+    // }
+
+    // if (index == 1) {
+    //   this.head = current.nextNode
+    //   return this.toString();
+    // }
+
+    // while (current.nextNode != null) {
+    //   counter++;
+    //   previousNode = current;
+    //   current = current.nextNode;
+    //   if (counter == index) {
+    //     // console.log(`index to delete of ${value}:`, counter);
+    //     remainingList = current.nextNode
+    //     previousNode.nextNode = remainingList;
+    //     return this.toString();
+    //   }
+    // }
+    // console.log("not found");
+    // return "not found";
   }
 }
 
@@ -119,9 +215,24 @@ console.log("old", list);
 list.prepend("string4");
 list.append("string5");
 list.append("string6");
-list.getTail()
+list.getTail();
 list.append("string7");
-list.getTail()
+list.pop();
+list.pop();
+list.getTail();
+
 console.log("new with prepend", list);
-list.size()
+list.size();
 list.getHead();
+list.at(1);
+list.at(5);
+list.contains("string5"); //true
+list.find("string5");
+list.contains("string8"); //false
+list.toString();
+console.log("delete test");
+list.removeAt(7);
+// list.removeAt(3)
+// list.removeAt(2)
+// list.removeAt(2)
+// list.removeAt(1)
